e4064ed761c1925481ff4e5242ec943cad502086
josepht273 <josephtadessem@gmail.com>
2025-11-02 00:47:37 +0300

    enable file reading function

--- index.ts
+++ index.ts
@@ -1,30 +1,54 @@
-import bible from "./01_ኦሪት ዘፍጥረት.json";
-import db from "./config";
+import fs from "node:fs";
+import db, { bibleFiles } from "./config";
 import { bookName, verses } from "./db/schema";
 
-async function main() {
-	const _bibleName = await db
-		.insert(bookName)
-		.values({
-			name: bible.title,
-			chapters: bible.chapters.length,
-		})
-		.returning();
+interface VerseType {
+	verse: string;
+	text: string;
+}
 
-	bible.chapters.forEach((chapter) => {
-		chapter.verses.forEach(async (verse) => {
-			const v = await db
-				.insert(verses)
-				.values({
-					chapter: chapter.chapter.toString(),
-					text: verse.text,
-					verse: verse.verse,
-					bookID: _bibleName[0]?.id,
-				})
-				.returning();
-			console.log(v);
+interface ChaptersType {
+	chapter: string;
+	title: string;
+	verses: VerseType[];
+}
+interface FileReadingType {
+	title: string;
+	abbv: string;
+	chapters: ChaptersType[];
+}
+
+function readFile(filename: string): FileReadingType {
+	const raw = fs.readFileSync(`amharic_bible/${filename}`, "utf8");
+	return JSON.parse(raw);
+}
+
+async function main() {
+	for (const file of bibleFiles) {
+		const bible = readFile(file);
+		const _bibleName = await db
+			.insert(bookName)
+			.values({
+				name: bible.title,
+				chapters: bible.chapters.length,
+			})
+			.returning();
+
+		bible.chapters.forEach((chapter) => {
+			chapter.verses.forEach(async (verse) => {
+				await db
+					.insert(verses)
+					.values({
+						chapter: chapter.chapter.toString(),
+						text: verse.text,
+						verse: verse.verse,
+						bookID: _bibleName[0]?.id,
+					})
+					.returning();
+			});
 		});
-	});
+		console.log(`${_bibleName[0]?.name} is done`);
+	}
 }
 
 main();
