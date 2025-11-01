262f30fce309e36fb3c30634bae0d1f448a5ceb5
josepht273 <josephtadessem@gmail.com>
2025-11-02 00:34:24 +0300

    configure the json to sqlite converting function

--- db/schema.ts
+++ db/schema.ts
@@ -3,13 +3,13 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const bookName = sqliteTable("book_name", {
 	id: int().primaryKey({ autoIncrement: true }),
 	name: text().notNull(),
-	chapters: text().notNull(),
+	chapters: int().notNull(),
 });
 
 export const verses = sqliteTable("verses", {
 	id: int().primaryKey({ autoIncrement: true }),
 	bookID: int().references(() => bookName.id, { onDelete: "cascade" }),
-	chapter: int().notNull(),
+	chapter: text().notNull(),
 	verse: text().notNull(),
 	text: text().notNull(),
 });
--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import bible from "./01_ኦሪት ዘፍጥረት.json";
+import db from "./config";
+import { bookName, verses } from "./db/schema";
+
+async function main() {
+	const _bibleName = await db
+		.insert(bookName)
+		.values({
+			name: bible.title,
+			chapters: bible.chapters.length,
+		})
+		.returning();
+
+	bible.chapters.forEach((chapter) => {
+		chapter.verses.forEach(async (verse) => {
+			const v = await db
+				.insert(verses)
+				.values({
+					chapter: chapter.chapter.toString(),
+					text: verse.text,
+					verse: verse.verse,
+					bookID: _bibleName[0]?.id,
+				})
+				.returning();
+			console.log(v);
+		});
+	});
+}
+
+main();
